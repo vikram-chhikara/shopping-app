24e21ecfb4e445f6b803c35e69d5b471eae1a1a3
PQ <pnquach@ucsd.edu>
2017-05-30 03:31:54 -0700

    Indexing tests and report started

--- CSE135_ShoppingApp/sqlScripts/indices.sql
+++ CSE135_ShoppingApp/sqlScripts/indices.sql
@@ -1,1 +1,27 @@
--- state query indices -- 
+/* Indexing */
+
+-- INDEX 1 -- 
+CREATE INDEX state_table_name ON state(state_name)
+DROP INDEX state_table_name
+
+-- INDEX 2 -- 
+CREATE INDEX person_table_state_id ON person(state_id)
+DROP INDEX person_table_state_id
+
+-- INDEX 3 -- 
+CREATE INDEX product_in_cart_prod_id ON products_in_cart(product_id)
+DROP INDEX product_in_cart_prod_id
+
+-- INDEX 4 -- 
+CREATE INDEX prod_in_cart_cart_id ON products_in_cart(cart_id)
+DROP INDEX prod_in_cart_cart_id
+
+-- INDEX 5 -- 
+CREATE INDEX shopping_cart_person_id ON shopping_cart(person_id)
+DROP INDEX shopping_cart_person_id
+
+-- INDEX 6 -- 
+CREATE INDEX product_table_cat_id ON product(category_id)
+DROP INDEX product_table_cat_id
+
+
--- CSE135_ShoppingApp/sqlScripts/sql_sales_analysis_queries.sql
+++ CSE135_ShoppingApp/sqlScripts/sql_sales_analysis_queries.sql
@@ -1,6 +1,6 @@
 /* SQL STATE QUERIES */
 
-/* INDEX 1 */
+/* QUERY 1 */
 -- Alphabetical and full sum of products per state with and without category filter --
 SELECT tot.state_name, SUM(tot.price) AS price 
 FROM (SELECT s.state_name, COALESCE((pr.price * quantity), 0) AS price 
@@ -11,7 +11,7 @@ GROUP BY tot.state_name ORDER BY state_name
 -- ORDER BY price DESC
 -- LIMIT 20 OFFSET ?
 
-/* INDEX 2 */
+/* QUERY 2 */
 -- Products per state and full pricing --
 SELECT product_name, COALESCE(SUM(pr.price*quantity),0) as price 
 FROM product p LEFT OUTER JOIN products_in_cart pr ON p.id = pr.product_id 
@@ -20,7 +20,7 @@ ORDER BY product_name
 -- ORDER BY price DESC
 -- LIMIT 10 OFFSET 
 
-/* INDEX 3 */
+/* QUERY 3 */
 -- Alphabetical product ordering with and without category filter
 SELECT product_name, SUM(pr.price*quantity) as price 
 FROM product p JOIN products_in_cart pr ON p.id = pr.product_id -- AND category_id = ? 
@@ -29,16 +29,7 @@ ORDER BY product_name
 -- ORDER BY price DESC
 -- LIMIT 10 OFFSET ?
 
-/* INDEX 4 */
--- Sum of products per state with and without limited by category --
-SELECT tot.state_name, SUM(tot.price) AS price 
-FROM (SELECT s.state_name, COALESCE((pr.price * quantity), 0) AS price 
-FROM (state s LEFT OUTER JOIN person p ON p.state_id = s.id) LEFT OUTER JOIN 
-((products_in_cart pc JOIN product pr ON pc.product_id = pr.id) 
-JOIN shopping_cart sc ON pc.cart_id = sc.id /* AND category_id = ? */) ON p.id = person_id) as tot 
-GROUP BY tot.state_name ORDER BY price DESC
-
-/* INDEX 5 */
+/* QUERY 4 */
 -- Basic state x product value --
 SELECT tot.state_name, product_name, SUM(tot.price) as price 
 FROM (SELECT s.state_name, pr.product_name, (pr.price * quantity) AS price 
@@ -47,39 +38,33 @@ FROM (state s JOIN person p ON p.state_id = s.id) JOIN
 JOIN shopping_cart sc ON pc.cart_id = sc.id) ON p.id = person_id) as tot 
 GROUP BY tot.state_name, product_name
 
-/* INDEX 6 */
+/* QUERY 5 */
 -- Products per customer and full pricing with alphabetical sort --
 SELECT p.id, p.person_name, COALESCE(SUM(pi.price*pi.quantity),0) as price 
-FROM (person p LEFT OUTER JOIN shopping_cart s on  p.id = s.person_id) 
-LEFT OUTER JOIN (product pr LEFT OUTER JOIN products_in_cart pi ON 
-pr.id = pi.product_id) on s.id = pi.cart_id 
-GROUP BY p.id, p.person_name ORDER BY p.person_name;
+FROM (person p LEFT OUTER JOIN shopping_cart s ON p.id = s.person_id) 
+LEFT OUTER JOIN (product pr LEFT OUTER JOIN products_in_cart pi 
+ON pr.id = pi.product_id) on s.id = pi.cart_id
+GROUP BY p.id, p.person_name 
+ORDER BY p.person_name;
+-- ORDER BY price DESC
 -- LIMIT 20 OFFSET ?;
 
-/* INDEX 7 */
--- Person purchases sorted by top --
-SELECT p.id, p.person_name, COALESCE(SUM(pi.price*pi.quantity),0) as price 
-FROM (person p LEFT OUTER JOIN shopping_cart s on  p.id = s.person_id) 
-LEFT OUTER JOIN (product pr LEFT OUTER JOIN products_in_cart pi 
-ON pr.id = pi.product_id) on s.id = pi.cart_id GROUP BY p.id, p.person_name 
-ORDER BY price DESC 
--- LIMIT 20 OFFSET ?
-
-/* INDEX 8 */
+/* QUERY 6 */
 -- Person purchases with category filter --
 SELECT p.id, p.person_name, COALESCE(SUM(pi.price*pi.quantity),0) as price 
-FROM (person p LEFT OUTER JOIN shopping_cart s on  p.id = s.person_id) 
+FROM (person p LEFT OUTER JOIN shopping_cart s ON p.id = s.person_id) 
 LEFT OUTER JOIN (product pr JOIN products_in_cart pi 
-ON pr.id = pi.product_id AND category_id = ?) on s.id = pi.cart_id 
+ON pr.id = pi.product_id /*AND category_id = ?*/) on s.id = pi.cart_id 
 GROUP BY p.id, p.person_name 
 ORDER BY price DESC 
 -- person_name
 -- LIMIT 20 OFFSET ? --
 
-/* INDEX 9 */
+/* QUERY 7 */
 -- Basic Person x Product cell value
 SELECT p.id, p.person_name, pr.product_name, SUM(pi.price*pi.quantity) as price 
 FROM (person p LEFT OUTER JOIN shopping_cart s on  p.id = s.person_id) 
 JOIN (product pr JOIN products_in_cart pi 
-ON pr.id = pi.product_id) on s.id = pi.cart_id GROUP BY p.id, p.person_name, pr.product_name 
+ON pr.id = pi.product_id) on s.id = pi.cart_id 
+GROUP BY p.id, p.person_name, pr.product_name 
 ORDER BY p.person_name
